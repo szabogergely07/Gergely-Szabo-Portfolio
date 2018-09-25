00696557a1cc77fb341f02443f8542e9141a631b
greg <szabogergely07@gmail.com>
2018-09-25 15:44:40 +0200

    New project- todolist API

--- js/work.js
+++ js/work.js
@@ -28,8 +28,18 @@ var works = [
 		pic: "img/laravel.png",
 		url: "http://gregslaravelapp.herokuapp.com",
 		id: "works4",
-		specs: "Next to Ruby I am interested in the Php language too, so this project is my showcase for a fully working Laravel application, it manages users, posts, categories, comments. It is under construction.",
-		tools: "Php with Laravel Homestead, PhpMyAdmin for MySQL"
+		specs: "This project is my showcase for a fully working Laravel application, it manages users, posts, categories, comments. It is under construction, but you can follow my work.",
+		tools: "Php with Laravel Homestead, PhpMyAdmin for MySQL, Heroku for production <hr> <h4>Progress</h4>\
+                <p class=\"modal-project\">You can test my app in production if you login <a href=\"http://gregslaravelapp.herokuapp.com/login\" target=\"_blank\">HERE</a> with these credentials:<br>Email: greg@greg.com / Password: greg123 / Then click on Admin <br>\
+                The admin area was created first for the application. Here we can see the registered users, we can authorize them to be admin, so have full access. We can create posts and add a category to them. I am starting notes not from the very beginning, you can check the previous progress on my GitHub account. <br>\
+                10.04.2018 - Deploying to Heroku with creating a ClearDB Mysql databse, as I am running on Mysql in development. Here we have to create a different database.php file or the file with different settings for the connection to the database. I also created a partial for error messages by creating user/post etc.. <br>\
+                11.04. - In development I created a photo upload section, but on development I would need to start using Amazon Web Services (AWS) to store files as it is not possible on Heroku. I decided not to do this, for now I am using weblinks to show pictures and later on I will setup AWS.<br>\
+                12-14.04. - I am getting an error message from the browser in development: \"The page has expired\". Solution: change session driver to database instead of file in .env file.<br>Delete User function and middleware authorization (Admin) added.<br>\
+                18.04. - Flash messages added to CRUD and custom 404 page to view. Posts controller has been created, routes/views/model have been set.<br>Problem in dev: 'failed to open stream: Permission denied'<br>Solution: There is a permission problem in storage, the command 'chmod -R 777 app/storage' helps.<br>\
+                19.04 - PostRequest added for posts form validation. Categories CRUD is ready.<br>\
+                23.04 - Comment model and controller have been created with view and routes. View's being made dynamic.<br>\
+                21.05 - Creating sample users with factory seeding. I had to include the 'faker' dependency in composer.json in 'require' section and update composer in heroku bash. Then seeding has worked.\
+                </p>"
 	},
 	{
 		title: "VR Scene",
